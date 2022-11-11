f5185e689200d0084b19bdc532e9710a94b30248
Aline do Amaral <aline.amaral@vortigo.com.br>
2022-11-11 14:37:36 -0300

    new commit

---
+++ Rocket-Cinema/ProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileViewController.swift
+//  Rocket-Cinema
+//
+//  Created by Aline do Amaral on 11/11/22.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
