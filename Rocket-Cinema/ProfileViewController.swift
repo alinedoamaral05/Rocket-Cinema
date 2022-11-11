c7ee3f804f4b3e89136a9465606dd7d8c8a8116c
Aline do Amaral <aline.amaral@vortigo.com.br>
2022-11-11 15:18:54 -0300

    profile screen almost all set

--- Rocket-Cinema/ProfileViewController.swift
+++ Rocket-Cinema/ProfileViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    
+    @IBOutlet weak var profileNameTitleLabel: UILabel!
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var profileNameLabel: UILabel!
+    
+    @IBOutlet weak var profileContentLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
